4dedf25def1c21fbb36bee4df4af6f81d8e475d7
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-03-27 02:54:50 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,28 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int find(int[] array, int number) {
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] == number) return i;
+		}
+		return -1;
 	}
 
-	
+	public static int findLast(String[] array, String string) {
+		for (int i = array.length - 1; i >= 0; --i) {
+			if (array[i] == string) return i;
+		}
+		return -1;
+	}
+
+	public static int findSecond(char[] array, char character) {
+		int index = -1;
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] == character) {
+				if (index == -1) index = i;
+				else if (index != -1) return i;
+			}
+		}
+		return index;
+	}
 
 }
